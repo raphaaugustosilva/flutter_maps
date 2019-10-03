5034506bea4b669669b0edf58d0aec171461d55e
Raphael Augusto da Silva <raphaaugustosilva@outlook.com>
2019-10-03 17:56:17 -0300

    Implementando google maps + pedindo confirmacao para usuairo simples

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,9 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyC9YoJg5jN9EohyIjoqdbFO7_Ad9QRtfhg")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
+
--- lib/views/principalView.dart
+++ lib/views/principalView.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 
 class PrincipalView extends StatefulWidget {
   @override
@@ -6,10 +9,50 @@ class PrincipalView extends StatefulWidget {
 }
 
 class _PrincipalViewState extends State<PrincipalView> {
+  // Declare outside build method
+  GoogleMapController mapController;
+
+  @override void initState() {
+    //Map<PermissionGroup, PermissionStatus> permissions = await PermissionHandler().requestPermissions([PermissionGroup.contacts]);
+    PermissionHandler().requestPermissions([PermissionGroup.location]);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: GoogleMap(
+              onMapCreated: (GoogleMapController controller) {
+                mapController = controller;
+              },
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+              initialCameraPosition: CameraPosition(
+                target: LatLng(37.4219999, -122.0862462),
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        mapController.animateCamera(
+          // CameraUpdate.newCameraPosition(
+          //   CameraPosition(target: LatLng(37.4219999, -122.0862462), zoom: 20.0),
+          // ),
+          CameraUpdate.newLatLngBounds(
+            LatLngBounds(
+              southwest: LatLng(48.8589507, 2.2770205),
+              northeast: LatLng(50.8550625, 4.3053506),
+            ),
+            32.0,
+          ),
+        );
+      }),
     );
   }
 }
