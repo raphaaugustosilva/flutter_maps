9cdc3c655410f93c117d7657927b0fa4e8d1b127
Raphael Augusto da Silva <raphaaugustosilva@outlook.com>
2019-10-03 13:49:25 -0300

    Estrutura do projeto

---
+++ lib/views/principalView.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PrincipalView extends StatefulWidget {
+  @override
+  _PrincipalViewState createState() => _PrincipalViewState();
+}
+
+class _PrincipalViewState extends State<PrincipalView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
